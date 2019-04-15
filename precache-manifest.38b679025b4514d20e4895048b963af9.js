e4ef281af24ff42391ccd4bac11754b087598d03
jhnsartain3 <=>
2019-04-14 21:32:53 -0500

    Updates

---
+++ precache-manifest.38b679025b4514d20e4895048b963af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/johnsandroidstudiotutorials/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "737ee27b1da271535102",
+    "url": "/johnsandroidstudiotutorials/static/js/runtime~main.039d9faf.js"
+  },
+  {
+    "revision": "e017581cc63a8f3bffe2",
+    "url": "/johnsandroidstudiotutorials/static/js/main.fed64dd5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/johnsandroidstudiotutorials/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e017581cc63a8f3bffe2",
+    "url": "/johnsandroidstudiotutorials/static/css/main.905a5093.chunk.css"
+  },
+  {
+    "revision": "1b57a8ab8615047419bf2cc14f1dcbd7",
+    "url": "/johnsandroidstudiotutorials/index.html"
+  }
+];

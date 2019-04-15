2df400a13b34ca240383686111b284d48d75373b
jhnsartain3 <=>
2019-04-14 22:23:32 -0500

    Update information

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            John's Android studio Tutorials
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        John's Android Studio Tutorials
+                    </p>
+                    <p>
+                        Site is currently being built </p>
+                    <p>
+                        Check back later for updates
+                    </p>
+                    <p>
+                        Requests can be sent to </p>
+                    <p>
+                        johns.as.tutorials@gmail.com
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://www.youtube.com/c/JohnsAndroidStudioTutorialsvideos"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        John's Android Studio Tutorials
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
